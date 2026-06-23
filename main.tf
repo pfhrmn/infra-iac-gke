aabcd8e31c5fc9de45403814926fea042e9901ad
pfhrmn <2510781029@hochschule-burgenland.at>
2026-06-23 23:05:13 +0200

    feat: add GKE cluster and node pool

--- main.tf
+++ main.tf
@@ -8,3 +8,28 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
+resource "google_container_cluster" "gke" {
+  name     = "platform-cluster"
+  location = var.zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-pool"
+  cluster    = google_container_cluster.gke.name
+  location   = var.zone
+
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
