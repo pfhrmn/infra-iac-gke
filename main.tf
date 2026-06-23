f34f549893b99e8777a7b8f5a9be5b1abaedfa42
pfhrmn <2510781029@hochschule-burgenland.at>
2026-06-23 22:52:44 +0200

    feat: add VPC and subnet

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-// main.tf – wird später mit Terraform Code gefüllt
+resource "google_compute_network" "vpc" {
+  name = "platform-vpc"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "platform-subnet"
+  ip_cidr_range = "10.0.0.0/20"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
