2d7fa47b9b9ddecd2595490920cb4130741d0a8f
pfhrmn <2510781029@hochschule-burgenland.at>
2026-06-23 22:42:10 +0200

    feat: add terraform base structure

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  description = "GCP Region"
+  default     = "europe-west1"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  default     = "europe-west1-b"
+}
